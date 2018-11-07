7e967774345b7873413f5499ec5c09dbdcc66814
yacin123 <44523421+yacin123@users.noreply.github.com>
2018-11-07 19:15:09 +0100

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,57 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+
+‏نقدم لك دعوة لسموك الراقيَ .
+‏لنحظى بجمال حضورك وتواجدك
+•بتنـورونـا الجميع :wink:
+•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
+•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
+•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
+شرفنا يا عسل:wink: :kissing_heart:
+
+https://discord.gg/FTk3HY
+joined info-!! -- ♯  Étoiles ' Offical .
+
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
+╔╦╦╦═╦╗╔═╦═╦══╦═╗
+║║║║╩╣╚╣═╣║║║║║╩╣
+╚══╩═╩═╩═╩═╩╩╩╩═╝ 
+ **                                                                           :What: الجوائز هي كلاتالي
+
+المركز الاول : نيترو مدة شهر :right_facing_fist: 
+-
+المركز الثاني : 100 الف كريدت + 5 حسابات فل داتا 
+-
+المركز الثالث : فل اكسس   
+- 
+@everyone **
+
+https://discord.gg/FTk3HY
+
+joined info-- - ♯  Étoiles ' Offical .
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login('NTA0NDAyNjE1OTc5NzM3MDk5.DrSNDQ.VZqtYkIWkL0wrELdKnPpDBY4xWU');
